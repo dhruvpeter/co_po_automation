484fb9999302b2117730ad067ec16ccf49c7fe8e
Dhruv Peter <dhruveldhopeter@gmail.com>
2020-11-05 16:28:15 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Admin from './Component/Admin/Admin'
+import Faculty from './Component/Faculty/Faculty'
+import Login from './Component/Login/Login'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Admin/>
+      <Faculty/>
+      <Login/>
     </div>
-  );
+  )
 }
-
-export default App;
