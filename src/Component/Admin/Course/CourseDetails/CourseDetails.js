f591d42951b5a6d1efa6cfdfa8df7f1e9ff735d2
Dhruv Peter <dhruveldhopeter@gmail.com>
2020-11-20 18:46:18 +0530

    second filter added for course-details. admin/course-details completed

--- src/Component/Admin/Course/CourseDetails/CourseDetails.js
+++ src/Component/Admin/Course/CourseDetails/CourseDetails.js
@@ -9,42 +9,79 @@ export default function CourseDetails() {
       code: "PH100",
       name: "Engineering Physics",
       semester: "S1",
+      batchYear: "2020",
     },
     {
       code: "PH100",
       name: "Engineering Physics",
       semester: "S1",
+      batchYear: "2020",
     },
     {
       code: "PH100",
       name: "Engineering Physics",
       semester: "S1",
+      batchYear: "2022",
     },
     {
       code: "CS205",
       name: "Data Structures",
       semester: "S3",
+      batchYear: "2022",
     },
     {
       code: "CS301",
       name: "Theory of Computation",
       semester: "S5",
+      batchYear: "2022",
     },
   ];
   const semester = ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"];
-  const [filterTag, setFilterTag] = useState("S1");
-  const handleChangeFilterTag = (event) => {
-    setFilterTag(event.target.value);
+  const batchYear = [
+    "2019",
+    "2020",
+    "2021",
+    "2022",
+    "2023",
+    "2024",
+    "2025",
+    "2026",
+    "2027",
+    "2028",
+    "2029",
+    "2030",
+  ];
+  const [filterSem, setFilterSem] = useState("S1");
+  const handleChangeFilterSem = (event) => {
+    setFilterSem(event.target.value);
   };
-  const filteredCourse = course.filter(function (result) {
-    return result.semester === filterTag;
+  const [filterBatchYear, setFilterBatchYear] = useState("2022");
+  const handleChangeFilterBatchYear = (event) => {
+    setFilterBatchYear(event.target.value);
+  };
+  const filteredCourseWithSem = course.filter(function (course) {
+    return course.semester === filterSem;
+  });
+  const filteredCourseWithSemAndBatchYear = filteredCourseWithSem.filter(function (course) {
+    return course.batchYear === filterBatchYear;
   });
   return (
     <div className="container-fluid">
-      <div className="filter">
+      <div className="filter-sem">
         <label>
-          <select value={filterTag} onChange={handleChangeFilterTag}>
+          <select value={filterSem} onChange={handleChangeFilterSem}>
             {semester.map((data, index) => (
+              <option key={index} value={data}>
+                {data}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <div className="filter-batchYear">
+        <label>
+          <select value={filterBatchYear} onChange={handleChangeFilterBatchYear}>
+            {batchYear.map((data, index) => (
               <option key={index} value={data}>
                 {data}
               </option>
@@ -64,7 +101,7 @@ export default function CourseDetails() {
             </tr>
           </thead>
           <tbody>
-            {filteredCourse.map((data, index) => (
+            {filteredCourseWithSemAndBatchYear.map((data, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>
                 <td>{data.code}</td>
