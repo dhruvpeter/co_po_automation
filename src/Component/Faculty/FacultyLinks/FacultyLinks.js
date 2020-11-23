61327bfea155109fe15d1a0248abbb3722c8942e
Dhruv Peter <dhruveldhopeter@gmail.com>
2020-11-23 10:29:13 +0530

    nil

--- src/Component/Faculty/FacultyLinks/FacultyLinks.js
+++ src/Component/Faculty/FacultyLinks/FacultyLinks.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-<<<<<<< HEAD
-// import Home from "../Home/Home";
-=======
->>>>>>> e1345d482d919cc0e88838b560a77fa66155f8b5
 import ViewStudent from "../Student/ViewStudent/ViewStudent";
 import AddMark from "../Student/AddMark/AddMark";
 import AssignStudent from "../Student/AssignStudent/AssignStudent";
