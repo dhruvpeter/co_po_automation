c842e0e0fea87ad6745105777864b44ac0fb41fd
sheenxavi004 <sheenxavi004@gmail.com>
2020-12-01 18:45:54 +0530

    CO Error resolved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import Admin from "./Component/Admin/Admin";
-import SelectedCourse from "./Component/Admin/Course/CourseDetails/SelectedCourse/SelectedCourse";
-import SelectedFaculty from "./Component/Admin/Faculty/ViewFaculty/SelectedFaculty/SelectedFaculty";
-import CO from "./Component/Admin/Result/CO/CO";
-import PSO from "./Component/Admin/Result/PSO/PSO";
-import EditStudent from "./Component/Admin/Student/ViewStudent/EditStudent/EditStudent";
 
 export default function App() {
   return (
     <div>
-      <CO/>
+      <Admin />
     </div>
   );
 }
