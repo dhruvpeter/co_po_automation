f5e5f48123aa65810257267c2c6f1a79a1cd1892
dhruvpeter <dhruveldhopeter@gmail.com>
2021-01-16 23:16:21 +0530

    Update internal1

--- src/Component/Faculty/Student/AddMark/Internal1/Internal1.js
+++ src/Component/Faculty/Student/AddMark/Internal1/Internal1.js
@@ -18,8 +18,8 @@ export default function Internal1() {
       const formData = new FormData(form.current);
 
       console.log(formData);
-      formData.append("courseCode", "CS301");
-      formData.append("year", "2020");
+      formData.append("courseCode", filterCourse.code);
+    //   formData.append("year", "2020");
       formData.append("batch", "2022");
       formData.append("internalExamNo", "1");
 
@@ -33,13 +33,40 @@ export default function Internal1() {
       console.log(err);
     }
   }
-
+  const faculty = {
+    id: "TVE18",
+    name: "Sreelal",
+    course: [
+      { code: "PH100", name: "Physics" },
+      { code: "CH", name: "Chemistry" },
+    ],
+  };
+  const [filterCourse, setFilterCourse] = useState(faculty.course[0]);
+  const handleChangeFilterCourse = (event) => {
+    setFilterCourse(event.target.value);
+  };
   return (
     <div id="container-fluid">
       <div>
         <h3>Upload Internal 1 Marks</h3>
       </div>
+      <div>
+        <h5>Choose course:</h5>
+        <label>
+          <select value={filterCourse.name} onChange={handleChangeFilterCourse}>
+            {faculty.course.map((data, index) => (
+              <option key={index} value={data}>
+                {data.name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
       <form ref={form} onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="year">Enter year: </label>
+          <input type="text" name="year" required />
+        </div>
         <input type="file" name="file" onChange={handleUpload} />
         <button type="submit" onClick={handleSubmit}>
           Submit
