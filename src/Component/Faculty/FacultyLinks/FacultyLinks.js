56ae0456dded8b9a04bb4f26d5e7d1c7a97f481c
dhruvpeter <dhruveldhopeter@gmail.com>
2021-01-16 22:49:58 +0530

    Add links to addmark

--- src/Component/Faculty/FacultyLinks/FacultyLinks.js
+++ src/Component/Faculty/FacultyLinks/FacultyLinks.js
@@ -6,12 +6,24 @@ import AddMark from "../Student/AddMark/AddMark";
 import AssignStudent from "../Student/AssignStudent/AssignStudent";
 import SurveyPO from "../Survey/SurveyPO/SurveyPO";
 import SurveyPSO from "../Survey/SurveyPSO/SurveyPSO";
+import Internal1 from "../Student/AddMark/Internal1/Internal1";
+import Internal2 from "../Student/AddMark/Internal2/Internal2";
+import Assignment1 from "../Student/AddMark/Assignment1/Assignment1";
+import Assignment2 from "../Student/AddMark/Assignment2/Assignment2";
+import FinalSem from "../Student/AddMark/FinalSem/FinalSem";
+
+
 export default function FacultyLinks() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/faculty/student/add-mark" exact component={AddMark} />
+        <Route path="/faculty/student/add-mark/internal-1" exact component={Internal1} />
+        <Route path="/faculty/student/add-mark/internal-2" exact component={Internal2} />
+        <Route path="/faculty/student/add-mark/assignment-1" exact component={Assignment1} />
+        <Route path="/faculty/student/add-mark/assignment-2" exact component={Assignment2} />
+        <Route path="/faculty/student/add-mark/final-sem" exact component={FinalSem} />
         <Route
           path="/faculty/student/view-student"
           exact
--- src/Component/Faculty/Student/AddMark/AddMark.js
+++ src/Component/Faculty/Student/AddMark/AddMark.js
@@ -1,124 +1,36 @@
-import React, { useState, useRef, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from 'axios'
-
-// import React from "react";
-
-/**
- * Component to handle file upload. Works for image
- * uploads, but can be edited to work for any file.
- */
-function AddMark() {
-  // State to store uploaded file
-  const [file, setFile] = React.useState("");
-
-  // Handles file upload event and updates state
-  function handleUpload(event) {
-    setFile(event.target.files[0]);
-
-    // Add code here to upload file to server
-    // ...
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(file);
-    axios.post('http://localhost:5000/')
-  }
-
-  // <p>Filename: {file.name}</p>
-      // <p>File type: {file.type}</p>
-      // <p>File size: {file.size} bytes</p>
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+export default function AddMark() {
   return (
-    <div id="upload-box">
-      <input type="file" onChange={handleUpload} />
-      <button type="submit" onClick={handleSubmit}>Submit</button>  
-      {/* {file && <ImageThumb image={file} />} */}
+    <div className="container-fluid">
+      <div>
+        <h3>Add Marks</h3>
+      </div>
+      <div>
+        <Link to="/faculty/student/add-mark/internal-1">
+          Upload Internal 1 marks
+        </Link>
+      </div>
+      <div>
+        <Link to="/faculty/student/add-mark/internal-2">
+          Upload Internal 2 marks
+        </Link>
+      </div>
+      <div>
+        <Link to="/faculty/student/add-mark/assignment-1">
+          Upload Assignment 1 marks
+        </Link>
+      </div>
+      <div>
+        <Link to="/faculty/student/add-mark/assignment-2">
+          Upload Assignment 2 marks
+        </Link>
+      </div>
+      <div>
+        <Link to="/faculty/student/add-mark/final-sem">
+          Upload Final Sem marks
+        </Link>
+      </div>
     </div>
   );
 }
-
-/**
- * Component to display thumbnail of image.
- */
-// const ImageThumb = ({ image }) => {
-//   return <img src={URL.createObjectURL(image)} alt={image.name} />;
-// };
-
-// It's not clear to me how to trigger updates to the UI
-// const useForceUpdate = () => useState()[1];
-
-// function AddMark() {
-//   const fileInput = useRef(null);
-//   const forceUpdate = useForceUpdate();
-
-//   useEffect((e) => {
-//     window.addEventListener("keyup", clickFileInput);
-//     return () => window.removeEventListener("keyup", clickFileInput);
-//   });
-
-//   function clickFileInput(e) {
-//     if (fileInput.current.nextSibling.contains(document.activeElement)) {
-//       // Bind space to trigger clicking of the button when focused
-//       if (e.keyCode === 32) {
-//         fileInput.current.click();
-//       }
-//     }
-//   }
-
-//   function onSubmit(e) {
-//     e.preventDefault();
-//     console.log("Submit Button clicked.");
-//   }
-//   const faculty = {
-//     id: "TVE18",
-//     name: "Sreelal",
-//     course: ["Physics", "Chemistry"],
-//   };
-//   const [filterCourse, setFilterCourse] = useState(faculty.course[0]);
-//   const handleChangeFilterCourse = (event) => {
-//     setFilterCourse(event.target.value);
-//   };
-//   return (
-//     <div className="container-fluid">
-//       <div>
-//         <h3>Add Marks</h3>
-//       </div>
-//       <div>
-//         <h5>Choose course:</h5>
-//         <label>
-//           <select value={filterCourse} onChange={handleChangeFilterCourse}>
-//             {faculty.course.map((data, index) => (
-//               <option key={index} value={data}>
-//                 {data}
-//               </option>
-//             ))}
-//           </select>
-//         </label>
-//       </div>
-//       <form onSubmit={onSubmit}>
-//         <input
-//           id="file"
-//           type="file"
-//           ref={fileInput}
-//           // The onChange should trigger updates whenever
-//           // the value changes?
-//           // Try to select a file, then try selecting another one.
-//           onChange={forceUpdate}
-//           multiple
-//         />
-//         <br />
-//         <br />
-
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<AddMark />, rootElement);
-
-export default AddMark;
