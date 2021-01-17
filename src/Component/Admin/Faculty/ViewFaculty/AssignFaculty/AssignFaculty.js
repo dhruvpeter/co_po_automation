480931edadfcfc79fd46c7f3a62ca34bd0d83e4f
psdhanesh7 <psdhanesh2000@gmail.com>
2021-01-18 00:15:02 +0530

    assign faculty completed

--- src/Component/Admin/Faculty/ViewFaculty/AssignFaculty/AssignFaculty.js
+++ src/Component/Admin/Faculty/ViewFaculty/AssignFaculty/AssignFaculty.js
@@ -2,45 +2,58 @@ import React, { useState, useEffect } from "react";
 import { Table, Form, Button } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 export default function AssignFaculty() {
-  console.log(useLocation().state);
+  const data = useLocation().state;
   const [faculty, setFaculty] = useState({
-    id: "1000",
-    name: "Sumesh Divakaran",
-    course: [
-      {
-        name: "Engineering Physics",
-        code: "AAA",
-        semester: "S1",
-        year: "2018",
-      },
-    ],
+    id: data.faculty_id,
+    name: data.faculty_name,
   });
+
+  const [filterSem, setFilterSem] = useState(1);
+
   //   const faculty = {
   //     id: "1000",
   //     name: "Sumesh Divakaran",
   //     course: [],
   //   };
 
-  const [course, setCourse] = useState([]);
+  const [course, setCourse] = useState([{
+    code: "PH100",
+    name: "Physics"
+  }]);
   useEffect(() => {
+    async function fetchCourse() {
+      try {
+        console.log(filterSem)
+        const res = await axios.get(`http://localhost:5000/api/course?semester=${filterSem}`)
+        // console.log(res.data.courses);
+        const courseList = [];
+        res.data.courses.forEach(course => courseList.push({code: course.course_code, name: course.course_name }))
+        console.log(courseList)
+        setCourse(courseList)
+      } catch(err) {
+        console.log(err);
+      }
+    }
 
-  }, [])
+    fetchCourse();
+  }, [filterSem])
+
   const year = [2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025];
   const [filterYear, setFilterYear] = useState(2018);
   const handleChangeFilterYear = (event) => {
     setFilterYear(event.target.value);
   };
   const semester = [1, 2, 3, 4, 5, 6, 7, 8];
-  const [filterSem, setFilterSem] = useState(1);
   const handleChangeFilterSem = (event) => {
     setFilterSem(event.target.value);
   };
 
   const [selectedCourse, setSCourse] = useState();
   const handleChangeCourse =  (event) => {
-    setCourse(event.target.value);
+    setSCourse(event.target.value);
   }
 
   const selectCourse = (event) => {
@@ -61,9 +74,28 @@ export default function AssignFaculty() {
 
     setFaculty({ ...faculty, course: courseAfterDelete });
   };
-  function Submit(e) {
+  async function Submit(e) {
     e.preventDefault();
-    console.log("Submit Button clicked.");
+    // console.log("Submit Button clicked.");
+    // console.log(selectedCourse);
+    // console.log(filterSem);
+    // console.log(filterYear);
+    // console.log(faculty.id)
+    // console.log(fil)
+    try {
+      const res = await axios.post(
+        'http://localhost:5000/api/course/assignfaculty',
+        {
+          courseCode: selectedCourse,
+          facultyId: faculty.id,
+          courseYear: filterYear,
+          passoutYear: 4 - (filterSem/2) + filterYear
+        }
+      )
+      console.log(res);
+    } catch(err) {
+      console.log(err);
+    }
   }
 
   function Back(e) {
@@ -77,10 +109,10 @@ export default function AssignFaculty() {
       </div>
       <div className="form">
         <Form>
-          <Form.Group>
+          {/* <Form.Group>
             <Form.Label>Faculty Id</Form.Label>
             <Form.Control type="text" name="id" value={faculty.id} readOnly />
-          </Form.Group>
+          </Form.Group> */}
           <Form.Group>
             <Form.Label>Faculty name</Form.Label>
             <Form.Control type="text" name="id" value={faculty.name} readOnly />
@@ -112,48 +144,17 @@ export default function AssignFaculty() {
               Select Course
             </option>
             {course.map((data, index) => (
-              <option key={index} value={data.name}>
+              <option key={index} value={data.code}>
                 {data.name}
               </option>
             ))}
           </select>
         </label>
-        <Button variant="dark" type="submit" onClick={selectCourse}>
-          Select
-        </Button>
+        {/* <Button variant="dark" type="submit" onClick={selectCourse}>
+          Assign
+        </Button> */}
       </div>
-      <div className="table">
-        <Form.Label>Selected Courses</Form.Label>
-        <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Semester</th>
-              <th>Course Name</th>
-              <th>Year</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {faculty.course.map((data, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{data.semester}</td>
-                <td>{data.name}</td>
-                <td>{data.year}</td>
-                <td>
-                  <Button variant="light" onClick={() => deleteItem(index)}>
-                    {/* <Link to="/admin/course/course-details/selected-course">
-                      View
-                    </Link> */}
-                    Remove
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
+      
       <div>
         <Button variant="dark" onClick={Back}>
           <Link to="/admin/faculty/view-faculty">Back</Link>
