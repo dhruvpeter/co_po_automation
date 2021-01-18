4dd908976dfb37e258418cae4deff63321bad334
dhruvpeter <dhruveldhopeter@gmail.com>
2021-01-18 07:15:33 +0530

    nil

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import Login from "./Component/Login/Login";
 export default function App() {
   return (
     <div>
-      <Admin />
-<<<<<<< HEAD
-=======
-      {/* <Faculty/> */}
-      {/* <Login/> */}
->>>>>>> cbfd60a34e84906c0a7932dd14c825c2d6688fb9
+      <Faculty />
     </div>
   );
 }
